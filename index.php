00e0e74f69bbb9e4b2876b279813c4228528aadd
ajsbsd <ajsbsd@gmail.com>
2026-05-16 19:54:47 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,38 @@
+Arizona machine-gun probe prompts dispute over seized Maryland gun collection
+Author
+By Van Smith
+PUBLISHED: June 6, 2012 at 3:00 AM EDT | UPDATED: June 28, 2019 at 10:12 AM EDT
 
+It’s been nearly seven years
+
+since 58-year-old auto mechanic and gun collector David Bord made a purchase that has caused him no end of grief: a Hatton Industries machine gun, which he got at the Armory, a gun store near Annapolis, in exchange for 10 pistols and $10,000. What Bord didn’t learn until almost three years later—in spring 2008, when agents of the federal Bureau of Alcohol, Tobacco, and Firearms (ATF) visited him—was that the gun was suspected of being part of an allegedly fraudulent gun-registration ring based in Arizona.
+
+In December 2009, as a result of an ATF tip involving Bord’s purchase at the Armory, Baltimore County police raided Bord’s home and business and seized a large part of his gun collection. Bord says they took 25 firearms worth $250,000, nearly all of them 35 to 50 years old, based on a faulty warrant, and that the guns are now in ATF’s possession.
+
+“They said the guns weren’t registered with Maryland or the [United States],” Bord says of the Baltimore County police who took his guns, “but every single one of the guns is—I showed them the paperwork. They know everything is legal, and they still refuse to return them. Every single one of them is legal and registered, but the idiots still won’t back down.”
+
+Bord’s ongoing efforts to have the guns returned to him include a lawsuit against Baltimore County, which is scheduled for trial in September, having survived the county’s attempt last year to have it dismissed. In addition, Bord says he asked the Maryland U.S. Attorney’s Office to file a federal lawsuit seeking to keep two of the guns, which it did in April; Bord hopes this strategy will mean a federal judge will review the legal issues involved in the guns’ return, and that he will prevail.
+
+Meanwhile, in 2010, Hal Paul Goldstein—the owner of the Armory—and another Maryland licensed firearms dealer, Randolph Benjamin Rodman, were charged in a 39-page, 107-count federal indictment in Arizona, along with four Arizona dealers: George Dibril Clark III, Lorren Marc Kalish, James Patrick Arnberger, and Idan C. Greenberg (“Blunderbusted,”
+
+Mobtown Beat
+
+, Aug. 5, 2010). The six stand accused of “harvesting” serial numbers from older machine guns made before May 19, 1986, and welding the numbers onto newer machine guns they had manufactured, in order to evade a federal machine-gun ban that prohibits citizens from possessing or transferring machine guns made after 1986.
+
+The Arizona case has been fraught with battles between the government and the defendants, who have claimed prosecutors have not been forthcoming with required disclosures of evidence. During an August 2011 hearing, U.S. District Judge Roslyn O. Silver, after hearing of evidence-sharing problems experienced by the defendants, said, “I’m going to consider dismissing the case” if the issues persist, according to a court transcript. A jury trial is scheduled for August.
+
+The 34 machine guns at issue in the Arizona indictment include the Hatton Industries weapon that Bord purchased from Goldstein, according to court records.
+
+In addition to the Arizona charges, Goldstein was indicted in Maryland federal court in December, charged with unlawful possession of machine guns and firearms. On May 24, Goldstein filed a court notice consenting to having the Maryland case transferred to Arizona, where he intends to plead guilty to a single count of improper firearms record-keeping.
+
+Goldstein’s attorney, Joseph Conte, declined to comment for this article, as did the Maryland U.S Attorney’s Office, whose spokesperson, Marcia Murphy, wrote in an e-mail that “since there is litigation pending, I don’t think we will have comment on either” the criminal charges or the forfeiture case involving Bord’s weapons. Bord, though, is openly indignant about his predicament. As a collector for more than 30 years, he says “my firearms paperwork is beyond reproach,” and he’s aghast that, despite the fact that he produced proof that his weapons were registered with the proper authorities, law enforcers continue to keep his property.
+
+Bord says ATF agents like to say that their agency’s acronym stands for “Always Think Forfeiture,” since taking and keeping people’s guns is an important part of their law-enforcement strategy. (Indeed, a few years ago, ATF suffered a dustup when it ordered Leatherman tools for its trainees inscribed with the words “Always Think Forfeiture,” then recalled the order after a member of Congress complained.)
+
+Bord’s lawsuit against Baltimore County over the seizure of his guns asserts that the two police officers named as defendants—Detective Erik Socha and Cpl. Anthony Kidwell—undertook the raids on Bord’s home and business based on faulty warrants “in order to intimidate” Bord and cause him “economic injury,” but also “for gratuitous sport rather than for proper law-enforcement purposes,” according to court documents. The lawsuit also contends that Socha and Kidwell, when presented with Bord’s firearms paperwork after the raids, said “save it for court,” that the paperwork was “wrong” or “bull___t,” and “words to the effect” that Bord “would never get his property back.”
+
+The county’s filings in the case, meanwhile, say the police and ATF are keeping the weapons while they continue “their investigation to determine if the weapons. . .are a violation of state or federal law.” Bord was accused of a misdemeanor firearms offense in Baltimore County Circuit Court after the raids, but in 2011 prosecutors tabled the charges.
+
+Bord’s wife, Robbin Bord, says “we are law-abiding businesspeople,” but that their experience with the gun seizure “has just about destroyed our faith in our government.” She says that her husband’s seized gun collection “was a significant asset—that was part of our retirement,” and that the government “shouldn’t waste time on going after legal people like us,” but instead “should go after illegals, the people who sell guns out of the back of their car.” She adds that “we are not political,” and that they are not members of any gun-rights group. Collecting “museum-quality pieces” kept “locked in safes” is “my husband’s hobby,” she continues, “and he likes it.”
+
+“I thought it was supposed to be ‘innocent until proven guilty,'” David Bord says, “but in this instance, it’s guilty until proven innocent.”
